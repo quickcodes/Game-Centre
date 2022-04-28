8653d3f034bee4daa6322e4cc591eb33e737c379
Dhruv Soni <dhruvsoni7220@gmail.com>
2022-04-28 17:53:40 +0530

    done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
